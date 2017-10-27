cb439cf25aafb82aa0aad72cccfcec4e9e48f09f
Yan Mingzhi <raindust@outlook.com>
2017-10-27 14:03:47 +0800

    添加设置属性示例

---
+++ include/utils.h
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "GL/gl3w.h"
+
+const GLchar* ReadShader(const char* filename)
+{
+#ifdef WIN32
+	FILE* infile;
+	fopen_s(&infile, filename, "rb");
+#else
+	FILE* infile = fopen(filename, "rb");
+#endif // WIN32
+
+	if (!infile) {
+#ifdef _DEBUG
+		std::cerr << "Unable to open file '" << filename << "'" << std::endl;
+#endif /* DEBUG */
+		return NULL;
+	}
+
+	fseek(infile, 0, SEEK_END);
+	int len = ftell(infile);
+	fseek(infile, 0, SEEK_SET);
+
+	GLchar* source = new GLchar[len + 1];
+
+	fread(source, 1, len, infile);
+	fclose(infile);
+
+	source[len] = 0;
+
+	return const_cast<const GLchar*>(source);
+}
