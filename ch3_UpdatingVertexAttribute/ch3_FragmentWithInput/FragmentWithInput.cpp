556dcd83c47db646087e0c9e178a56b3abe6199c
Yan Mingzhi <raindust@outlook.com>
2017-10-29 17:00:12 +0800

    添加interface block示例

---
+++ ch3_UpdatingVertexAttribute/ch3_FragmentWithInput/FragmentWithInput.cpp
@@ -0,0 +1,70 @@
+#include <sb7.h>
+#include <../include/utils.h>
+
+GLuint compile_shaders(void)
+{
+	// Create and compile vertex shader
+	const GLchar* vertexSource = ReadShader("triangle.vert");
+	GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertexSource, NULL);
+	glCompileShader(vertex_shader);
+	// Create and compile fragment shader
+	const GLchar* fragmentSource = ReadShader("triangle.frag");
+	GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragmentSource, NULL);
+	glCompileShader(fragment_shader);
+	// Create program, attach shaders to it, and link it
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+	// Delete the shaders as the program has them now
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+	return program;
+}
+
+class FirstTriangle_app : public sb7::application
+{
+public:
+	// <snip>
+	void startup() override
+	{
+		rendering_program = compile_shaders();
+		glGenVertexArrays(1, &vertex_array_object);
+		glBindVertexArray(vertex_array_object);
+	}
+
+	void shutdown() override
+	{
+		glDeleteVertexArrays(1, &vertex_array_object);
+		glDeleteProgram(rendering_program);
+		glDeleteVertexArrays(1, &vertex_array_object);
+	}
+
+	// Our rendering function
+	void render(double currentTime) override
+	{
+		static const GLfloat white[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+		glClearBufferfv(GL_COLOR, 0, white);
+		// Use the program object we created earlier for rendering
+		glUseProgram(rendering_program);
+		GLfloat attrib[] = { (float)sin(currentTime) * 0.5f,
+			(float)cos(currentTime) * 0.6f,
+			0.0f, 0.0f };
+		const GLfloat color[] = { (float)sin(currentTime) * 0.5f + 0.5f,
+			(float)cos(currentTime) * 0.5f + 0.5f,
+			0.0f, 1.0f };
+		// Update the value of input attribute 0
+		glVertexAttrib4fv(0, attrib);
+		glVertexAttrib4fv(1, color);
+		// Draw one triangle
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+	}
+
+private:
+	GLuint rendering_program;
+	GLuint vertex_array_object;
+};
+
+DECLARE_MAIN(FirstTriangle_app)
